03b3e4838310792d603b5e98d15c7ac26a10631f
omair <omairdal@gmail.com>
2022-08-31 19:50:28 +0300

    finished

--- src/App.tsx
+++ src/App.tsx
@@ -3,25 +3,39 @@ import classes from "./App.module.css";
 
 function App() {
   return (
-    <main className={classes.main}>
-      <header>
-        <h1>
-          Join
-          <br />
-          Hustle
-          <br />
-          Control
-        </h1>
-        <span>
-          If you want to control the Hustle, meet hustlers, track goals and
-          analyze your life circumstances with a realistic approach join us
-        </span>
-      </header>
-      <button>Join Us</button>
-      <footer>
-        Already have an account? <span>Log in</span>
-      </footer>
-    </main>
+    <>
+      <div className={classes.planet}>
+        <div className={classes.animatedwrapper}>
+          <img src='./kmainwookopya1.png' alt='planet 1' />
+          <img src='./kmdarkerwoo1kopya1.png' alt='planet 2' />
+          <img src='./kmlowestwookopya1.png' alt='planet 3' />
+        </div>
+      </div>
+      <div className={classes["right-circle"]}></div>
+      <div className={classes["left-circle"]}></div>
+      <main className={classes.main}>
+        <header>
+          <h1>
+            Join
+            <br />
+            Hustle
+            <br />
+            Control
+          </h1>
+          <p>
+            If you want to control the Hustle, meet hustlers, track goals and
+            analyze your life circumstances with a realistic approach, join us!
+          </p>
+        </header>
+        <a href='https://apps.apple.com/tr/app/hustlecontrol/id1641096356'>
+          <img
+            src='https://miro.medium.com/max/600/1*xqT83bMEz92IBYxS9UQNow.png'
+            alt='download from AppStore button'
+          />
+        </a>
+        <footer>Telif Hakki © 2022 HustleControl - Tum Haklari Saklidir</footer>
+      </main>
+    </>
   );
 }
 
